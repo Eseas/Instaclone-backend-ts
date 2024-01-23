6c8ffe0ae38f7430478f008a51a791e386687d6e
LSY <8snlf9@gmail.com>
2024-01-23 16:15:37 +0900

    2024:01:23:16

--- server.js
+++ server.js
@@ -1,53 +1,11 @@
 import {
     ApolloServerPluginLandingPageGraphQLPlayground,
     } from  "apollo-server-core";
-import {ApolloServer, gql} from "apollo-server";
-import {PrismaClient} from "@prisma/client";
-
-const client = new PrismaClient();
-
-const typeDefs = gql`
-type Movie {
-    id: Int!
-    title : String!
-    year : Int!
-    genre : String
-    createdAt : String!
-    updatedAt : String!
-}
-    type Query {
-        movies: [Movie]
-        movie(id: Int!): Movie
-    }
-    type Mutation {
-        createMovie(title:String!, year:Int!, genre:String): Movie
-        deleteMovie(title:String!): Boolean
-    }
-`;
-
-const resolvers = {
-    Query: {
-        movies: () => client.movie.findMany(),
-        movie: (_, {id}) => ({"title": "Hello", year:2021}),
-    },
-    Mutation: {
-        createMovie: (_, {title, year, genre}) => 
-            client.movie.create({
-              data: {
-                title,
-                year,
-                genre,
-              }
-            }),
-        deleteMovie: (_, {id}) => {
-            return true
-        },
-    }
-};
+import {ApolloServer} from "apollo-server";
+import schema from "./schema.js";
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    schema,
     plugins: [
         ApolloServerPluginLandingPageGraphQLPlayground(),
         ],
